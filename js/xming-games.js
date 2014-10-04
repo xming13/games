cb06e1f77a8bd88b850f50659c64ccdb15f624da
xming13 <xiaoming33@gmail.com>
2014-10-04 12:16:59 +0800

    add icons for mushrooms

--- js/xming-games.js
+++ js/xming-games.js
@@ -10,3 +10,5 @@ xming_preload_images[2].src = "https://xming13.github.io/games/images/icons/icon
 xming_preload_images[3].src = "https://xming13.github.io/games/images/icons/icon-catch-a-falling-star-hover.png";
 xming_preload_images[4].src = "https://xming13.github.io/games/images/icons/icon-spot-the-special-one.png";
 xming_preload_images[5].src = "https://xming13.github.io/games/images/icons/icon-spot-the-special-one-hover.png";
+xming_preload_images[6].src = "https://xming13.github.io/games/images/icons/icon-mushrooms.png";
+xming_preload_images[7].src = "https://xming13.github.io/games/images/icons/icon-mushrooms-hover.png";
